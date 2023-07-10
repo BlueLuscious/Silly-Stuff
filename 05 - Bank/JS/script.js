3c664407e95937a296a14bb6a44a375f66d72064
BlueLuscious <lucio.200152@gmail.com>
2023-07-10 05:00:18 -0300

    Try Class

---
+++ 05 - Bank/JS/script.js
@@ -0,0 +1,53 @@
+class Client {
+    constructor(firstname, salary) {
+        this.firstname = firstname
+        this.salary = salary
+    }
+
+    depositMoney(money) {
+        if (money > 0) {
+            this.salary += money
+            console.log(this.salary)
+        } else {
+            console.log('nope')
+        }
+    }
+
+    withdrawMoney(money) {
+        if (money > 0) {
+            if (cantidad <= this.salario) {
+                this.salary -= money
+                console.log(this.salary)
+            } else {
+                console.log('nope')
+            }
+        } else {
+            console.log('>0')
+        }
+    }
+
+}
+
+const form = document.getElementById("form")
+const send = document.getElementById('sendInputs')
+
+
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const firstname = document.getElementById('firstnameInput')
+    const salary = parseFloat(document.getElementById('salaryInput'))
+    const amount = parseFloat(document.getElementById('amountInput'))
+
+    const client1 = new Client(firstname.value, salary.value)
+
+    client1.depositMoney(amount)
+    console.log(client1)
+
+})
+
+
+
+
